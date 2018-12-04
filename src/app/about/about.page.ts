19357977664c4b152ee901434a053242a65b0aee
ARGENTINA\rodrigomartinezjr <junior.martinez@sdp.biz>
2018-12-04 15:34:49 +0100

    implemented photo gallery

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,32 +1,26 @@
 import { Component } from '@angular/core';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { PhotoService } from '../services/photo.service';
+import { Photo } from '../services/photo.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: 'about.page.html',
-  styleUrls: ['about.page.scss']
+  styleUrls: ['about.page.scss'],
+  providers: [PhotoService]
 })
 export class AboutPage {
-  currentImage: any;
 
-  constructor(private camera: Camera) {
+  constructor(private photoService: PhotoService) {
 
   }
 
   takePicture() {
-    const options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    }
+    this.photoService.takePicture();
+  }
 
-    this.camera.getPicture(options).then((imageData) => {
-      this.currentImage = 'data:image/jpeg;base64,' + imageData;
-    }, (err) => {
-      // Handle error
-      console.log("Camera issue:" + err);
-    });
+  get gallery(): Photo[] {
+    return this.photoService.photos;
   }
 
 }
